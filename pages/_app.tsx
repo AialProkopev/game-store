39dc23e48cdea6575dc97c4e84024e7702a313d3
Ayal <freezzforever@gmail.com>
2022-06-22 16:13:11 +0300

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.scss"
+import type { AppProps } from "next/app"
+import styles from "../styles/app.module.scss"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.app}>
+      <header className={styles.header}>header</header>
+      <aside className={styles.aside}>aside</aside>
+      <main className={styles.main}>
+        <Component {...pageProps} />
+      </main>
+    </div>
+  )
 }
 
 export default MyApp
