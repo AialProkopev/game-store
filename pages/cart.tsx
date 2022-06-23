eb61474637dbcc58604a79ad2c8af0b1895ca801
Ayal <freezzforever@gmail.com>
2022-06-23 12:14:04 +0300

    install dotenv

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,7 +1,10 @@
 import type { NextPage } from "next"
 import Head from "next/head"
+import { DotenvConfigOptions } from "dotenv"
 
 const Cart: NextPage = () => {
+  console.log(process.env.API_KEY)
+
   return (
     <div>
       <Head>
